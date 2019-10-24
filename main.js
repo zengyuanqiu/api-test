1d0dde935ee5cf5dfb7fbb02f86a7eefc1ea0482
zengyuanqiu <zengyuanqiu@gmail.com>
2019-10-24 20:23:29 +0800

    统计访问次数

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ var qs = require('qs')
 var axios = require('axios')
 var bp = require('body-parser')
 var count = 0
+var date = new Date()
+var day = date.getDate()
+
+var calcCurDayCount = function() {
+    var _date = new Date()
+    var y = _date.getFullYear()
+    var m = _date.getMonth() + 1
+    var d = _date.getDate()
+    var h = _date.getHours()
+    var mm = _date.getMinutes()
+    count = d === day ? count++ : 1
+    day = d
+    return count === 1 
+        ? `${y}-${m}-${d} ${h}:${mm}访问，总次数为：${count}` 
+        : `    ${h}:${mm}访问，总次数为：${count}`
+}
+
 
 app.use(exp.static(__dirname))
 app.use(bp.urlencoded({extended: false}))
@@ -15,7 +32,7 @@ app.use('/proxy', (req, res) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     count++
-    console.log('访问次数：' + count)
+    console.log(calcCurDayCount())
     let { body } = req
     if(!body.url) return res.send({error: 'Not find'})
  	axios(body).then(result => {
